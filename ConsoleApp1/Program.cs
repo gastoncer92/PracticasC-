0b5bf4639134a13ebaeda3236f208c2d936aebc5
Gastón <gastongithub@gmail.com>
2022-01-14 12:01:22 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Program
+        {
+            public static void Main()
+            {
+                //Imprimir un mensage en la consola
+                System.Console.WriteLine("Hola mundo!")
+            }
+        }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
